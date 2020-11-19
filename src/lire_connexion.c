4b19e656063bf6f3b2442124437f751bdf7237e7
serenayaacoub <72579572+serenayaacoub@users.noreply.github.com>
2020-11-19 14:28:02 +0100

    Update lire_connexion.c

--- src/lire_connexion.c
+++ src/lire_connexion.c
@@ -7,24 +7,33 @@
 #include "ligne.h"
 #include "ABR.h"
 
-void afficher_connexion(Une_connexion *head)
+typedef struct _nouv
+{
+    Un_nabr* sta_dep;
+    Un_nabr* sta_arr;
+    Une_ligne* ligne;
+    struct _nouv *suiv;
+} nouv;
+
+void afficher_connexion(nouv *head)
 {
     while (head)
     {
-        printf("\n station de départ : %s", head->sta_dep->data.sta.nom);
-        printf("\n station d'arriver : %s", head-> sta_arr->data.sta.nom);
+        printf("\n station de départ : %s",head->sta_dep->truc->data.sta.nom);
+        printf("\n station d'arriver : %s",head->sta_arr->truc->data.sta.nom);
         printf("\n ligne : %s", head->ligne->code);
-       // head = head -> suiv;
+        head = head -> suiv;
     }
 
 }
 
-Un_elem* lire_connexions(Une_ligne *liste_ligne, Un_nabr * abr_sta)
+Un_elem* lire_connexions()
 {
    
     //ouverture du fichier .csv;
     FILE* fichier ;
     fichier = fopen("connexion.csv", "r") ;
+            //on teste si l'ouverture du fichier est possible
     if (fichier == NULL)
     {
         printf("\n L'ouverture du fichier est impossible \n");
@@ -32,157 +41,97 @@ Un_elem* lire_connexions(Une_ligne *liste_ligne, Un_nabr * abr_sta)
     }
 
     //déclaration des variables nécessaires;
-    liste_ligne = (Une_ligne*) malloc (sizeof(Une_ligne));
-    abr_sta = (Un_nabr*) malloc(sizeof(Un_nabr));
-    Un_elem* ptr = (Un_elem*)malloc(sizeof(Un_elem));
+    /*Un_nabr* sta_dep = (Un_nabr*) malloc(sizeof(Un_nabr));
+    Un_nabr* sta_arr = (Un_nabr*) malloc(sizeof(Un_nabr));
+    Une_ligne* ligne = (Une_ligne*) malloc (sizeof(Une_ligne));*/
+    nouv* head = (nouv*) malloc(sizeof(nouv));
     
-    char* ptr_connex = NULL;
-    size_t size = 0;
+    //suite ;
+    /*Un_nabr* ptr_sta_dep = sta_dep;
+    Un_nabr* p_sta_dep = sta_dep;
+    Un_nabr* ptr_sta_arr = sta_arr;
+    Un_nabr* p_sta_arr = sta_arr;
+    Une_ligne* ptr_ligne = ligne;
+    Une_ligne* p_ligne = ligne;*/
+    nouv* phead = head;
+    nouv* pphead = head;
+    
+
+    //outils pour get line ;
+    char* ptr = NULL;
+    size_t size = 0;// l'utilisation de getline requiert un type size_t, que l'on initialise à 0
     char* line = NULL;
-    Un_elem* head = ptr;
     
-    Un_nabr* phead = abr_sta;
-    Une_ligne* pphead = liste_ligne;
     
     //on lit ligne par ligne ;
-    while(getline(&ptr_connex, &size, fichier) != -1)
+    while(getline(&ptr, &size, fichier) != -1)
     {
-        phead = abr_sta ;
-        line = NULL;
+        pphead = head;
         
-    //pour nabr : station dep / arr ;
-        
-        //pour sta_dep ;
-        line = strtok(ptr_connex, ";");
-        abr_sta -> truc = NULL;
-        abr_sta -> truc = (Un_truc*) malloc((strlen(line)+1)*sizeof(Un_truc));
+        //pour station de départ ;
+        line = strtok(ptr, ";");
+        head -> sta_dep = NULL;
+        head -> sta_dep = (Un_nabr*) malloc((strlen(line)+1)*sizeof(Un_nabr));
            
-        if (abr_sta -> truc == NULL)
+        if (head -> sta_dep == NULL)
         {
             printf("\n L'ouverture du fichier est impossible \n");
             exit(1);
         }
-        strcpy (abr_sta->truc->data.sta.nom, line);
-           
-        //pour sta_arr;
+        strcpy (head->sta_dep->truc->data.sta.nom , line);
+        
+        
+        //pour station d'arrivée ;
         line = strtok(NULL, ";");
-        abr_sta -> truc = NULL;
-        abr_sta -> truc = (Un_truc*) malloc((strlen(line)+1)*sizeof(Un_truc));
+        head -> sta_arr = NULL;
+        head -> sta_arr = (Un_nabr*) malloc((strlen(line)+1)*sizeof(Un_nabr));
          
-        if (abr_sta -> truc == NULL)
+        if (head -> sta_arr == NULL)
         {
             printf("\n L'ouverture du fichier est impossible \n");
+            fclose(fichier); 
             exit(1);
         }
-        strcpy (abr_sta->truc->data.sta.nom, line);
-        
-        
-    // pour ligne
+        strcpy (head->sta_arr->truc->data.sta.nom, line);
         
         //pour ligne ;
         line = strtok(NULL, ";");
-        pphead-> code= NULL;
-        pphead-> code = (char*) malloc((strlen(line)+1)*sizeof(char));
+        head -> ligne = NULL;
+        head -> ligne = (Une_ligne*) malloc((strlen(line)+1)*sizeof(Une_ligne));
               
-        if (pphead->code == NULL)
+        if (head -> ligne == NULL)
         {
             printf("\n L'ouverture du fichier est impossible \n");
+            fclose(fichier);
             exit(1);
         }
-        strcpy (pphead ->code, line);
-        //pour passer au suivant ;
-        pphead-> suiv = NULL;
-        pphead-> suiv= (Une_ligne*) malloc (sizeof (Une_ligne));
-        
-         if (pphead -> suiv == NULL)
-         {
-             printf("\n L'ouverture du fichier est impossible \n");
-             exit(1);
-         }
-         
-         pphead = pphead->suiv = NULL;
+        strcpy (head -> ligne -> code, line);
         
         
-    // pour passer au suivant;
-        ptr->truc = phead ->truc; /*pour pouvoir passer au suivant on passe par Un_elem*/
-        ptr-> suiv = NULL;
-        ptr-> suiv= (Un_elem*) malloc (sizeof (Un_elem));
+        // pour passer au suivant;
+        head-> suiv = NULL;
+        head-> suiv= (nouv*) malloc (sizeof (nouv));
             
-        if (ptr -> suiv == NULL)
+        if (head -> suiv == NULL)
         {
             printf("\n L'ouverture du fichier est impossible \n");
+            fclose(fichier);
             exit(1);
         }
-             
-        ptr = ptr->suiv = NULL;
+        head = head->suiv;
+        
      }
+    pphead->suiv = NULL;
+    
     //fermeture du fichier
     fclose(fichier);
-    return (head);
+    afficher_connexion(phead);
+    return (0);
         
 }
 int main()
 {
-    Un_elem* elem;
-    Une_ligne* ligne = NULL;
-    Un_nabr* sta = NULL;
-    elem = lire_connexions(ligne,sta);
-    
+    lire_connexions();
+
     return 0;
 }
-
-    
-    /*while(getline(&ptr_connex, &size, fichier) != -1)
-    {
-        phead = ptr ;
-        line = NULL;
-       
-        //pour sta_dep ;
-        line = strtok(ptr_connex, ";");
-        ptr -> truc = NULL;
-        ptr -> truc -> data.sta.nom = (Un_elem*) malloc((strlen(line)+1)*sizeof(Un_elem));
-       
-        if (abr_sta -> truc == NULL)
-        {
-            printf("\n L'ouverture du fichier est impossible \n");
-            exit(1);
-        }
-        strcpy (ptr->truc->data.sta.nom, line);
-       
-        //pour sta_arr;
-        line = strtok(NULL, ";");
-        ptr -> truc = NULL;
-        ptr -> truc = (Un_nabr*) malloc((strlen(line)+1)*sizeof(Un_nabr));
-     
-        if (ptr -> truc == NULL)
-        {
-            printf("\n L'ouverture du fichier est impossible \n");
-            exit(1);
-        }
-        strcpy (ptr->truc->data.sta.nom, line);
-    
-        //pour ligne ;
-        line = strtok(NULL, ";");
-        pphead -> code = NULL;
-        pphead-> code = (Une_ligne*) malloc((strlen(line)+1)*sizeof(Une_ligne));
-          
-        if (pphead->code == NULL)
-        {
-            printf("\n L'ouverture du fichier est impossible \n");
-            exit(1);
-        }
-        strcpy (pphead->code, line);
-    
-    
-        // pour passer sur le suivant;
-        phead-> suiv = NULL;
-        phead-> suiv= (Un_elem*) malloc (sizeof (Un_elem));
-       
-        if (phead -> suiv == NULL)
-        {
-            printf("\n L'ouverture du fichier est impossible \n");
-            exit(1);
-        }
-        
-        phead = phead->suiv = NULL;
-    }*/
