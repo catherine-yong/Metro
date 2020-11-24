9d62c92f1eb3293460719d5512b2e623b06e8a08
Catherine Yong <47810291+catherine-yong@users.noreply.github.com>
2020-11-24 19:37:30 +0100

    Add files via upload

--- include/liste.h
+++ include/liste.h
@@ -1,18 +1,6 @@
 #include "truc.h"
-#include <stdio.h>
 
 typedef struct _un_elem { 
     Un_truc *truc; /*Une station ou une connexion */
     struct _un_elem *suiv;
 } Un_elem; 
-
-Un_truc* extraire_deb_liste(Un_elem** liste)
-{
-    Un_elem* L = NULL;
-    if (*liste != NULL)
-    {
-        L = *liste;
-        *liste = (*liste)->suiv;
-    }
-    return L;
-}
