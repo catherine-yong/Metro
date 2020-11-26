3c4fbaa1b07e1e07fc3c3a14b256c1c105d660c0
Catherine Yong <47810291+catherine-yong@users.noreply.github.com>
2020-11-26 14:13:44 +0100

    Add files via upload

--- src/abr.c
+++ src/abr.c
@@ -1,18 +1,19 @@
-#include "arbr_type.h"
-#include "aqrtopo.h"
+
 #include "liste.h"
-#include "truc.h"
 #include "station.h"
+#include "arbr_type.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <string.h>
 
 Un_nabr *creer_nabr(Un_truc *truc)
 {
     /* on cree un noeud */
     Un_nabr *noeud = (Un_nabr *)malloc(sizeof(Un_nabr));
-    noeud->truc = truc;
+    noeud->truc = (Un_truc *)malloc(sizeof(Un_truc));
+    noeud-> truc = truc;
     noeud->g = NULL;
     noeud->d = NULL;
     return noeud;
@@ -22,77 +23,67 @@ Un_nabr *inserer_abr(Un_nabr *abr, Un_nabr *node)
 {
     /* on insere un noeud a sa place dans un ABR */
     /* on considere que notre ABR n'est composé que de stations, on ne traite pas le cas où il ya des connexions */
-
-    if(abr == NULL)
+    
+    if(!abr)
     {
-        abr = creer_nabr(node);
+        return node;
+    }
+    
+    if(strcmp((node->truc->data).sta.nom , (abr->truc->data).sta.nom) == 0)
+    {
+        /* si c'est egal alors c'est la racine , on n'insere rien*/
         return abr;
     }
     
-    if(node->truc->data.sta.nom == abr->truc->data.sta.nom)
-    {
-        /* si c'est egal alors c'est la racine */
-        return abr;
-    }
-    
-    if(strcmp(abr->truc->data.sta.nom , node->truc->data.sta.nom) > 0)
+    if(strcmp((node->truc->data).sta.nom , (abr->truc->data).sta.nom) > 0)
     {
         /* si c'est plus grand alors on place a droite */
+        
         abr->d = inserer_abr(abr->d,node);
     }
     
-    else
+    if(strcmp((node->truc->data).sta.nom , (abr->truc->data).sta.nom) < 0)
     {
         /* si c'est plus petit alors on place a gauche */
+        
         abr->g = inserer_abr(abr->g,node);
     }
-        
+    
+    
     return abr;   
 }
 
 Un_nabr *construire_abr(Un_elem *liste_sta)
 {
     Un_elem *ptr = liste_sta;
-    Un_elem *root = liste_sta;
-    Un_elem *head = NULL;
-    Un_elem *node;
+    Un_nabr *head = NULL;
+    head = (Un_nabr *) malloc(sizeof(Un_nabr));
+    if(!head)
+    {
+        printf("probleme allocation mémoire\n");
+        return NULL;
+    }
+
+    Un_nabr *node = NULL;
+    node = (Un_nabr *) malloc(sizeof(Un_nabr));
+    if(!node)
+    {
+        printf("probleme allocation mémoire\n");
+        return NULL;
+    }
 
     /* pour chaque element de la liste_sta, on crée un noeud que l'on insere dans la tete de l'arbre */
-
+    
     while(ptr != NULL)
     {
         node = creer_nabr(ptr->truc);
-        head = inserer_abr(root,node);
+        head = inserer_abr(head,node);
+        printf("nonnon\n");
         ptr = ptr->suiv;
     }
 
-    return root;
+    return head;
 
-}
-
-void detruire_abr(Un_nabr *abr)
-{
-    /* on crée une fonction recursive qui utilise d'autres fonctions intermédiaires */
-    if(abr != NULL)
-    {
-        detruire_branches(abr,abr->g);
-        detruire_branches(abr,abr->d);
-    }
-}
-
-void detruire_branches(Un_nabr *abr, Un_nabr *node)
-{
-    if(node->g)
-    {
-        detruire_branches(abr,abr->g);
-    }
-
-    if(node->d)
-    {
-        detruire_branches(abr,abr->d);
-    }
-
-    detruire_noeud(node);
 }
 
 void detruire_noeud(Un_nabr *node)
@@ -117,11 +108,49 @@ void detruire_noeud(Un_nabr *node)
     node = NULL;
 }
 
+void detruire_branches(Un_nabr *abr, Un_nabr *node)
+{
+    if(node->g)
+    {
+        detruire_branches(abr,abr->g);
+    }
+
+    if(node->d)
+    {
+        detruire_branches(abr,abr->d);
+    }
+
+    detruire_noeud(node);
+}
+
+void detruire_abr(Un_nabr *abr)
+{
+    /* on crée une fonction recursive qui utilise d'autres fonctions intermédiaires */
+    if(abr != NULL)
+    {
+        detruire_branches(abr,abr->g);
+        detruire_branches(abr,abr->d);
+    }
+}
+
+void affiche_abr(Un_nabr *abr)
+{
+    /* on affiche l'abr */
+    if(abr)
+    {
+        printf("\n nom %s\n",abr->truc->data.sta.nom);
+        affiche_abr(abr->d);
+        affiche_abr(abr->g);
+    }
+}
+
 Un_truc *chercher_station(Un_nabr *abr, char *nom)
 {
+    
     /* si l'arbre est vide, on retourne NULL */
     if(!abr)
     {
+        printf("La station n'est pas dans la liste\n");
         return NULL;
     }
 
@@ -140,7 +169,7 @@ Un_truc *chercher_station(Un_nabr *abr, char *nom)
     else
     {
         /* dans ce cas, le noeud recherché est simplement la racine */
-        return abr;
+        //printf("La station est bien dans la liste\n");
+        return abr->truc;
     }
 }
-
--- src/main.c
+++ src/main.c
@@ -11,79 +11,85 @@
 
 int main(void)
 {
-    // -- Exercice 1 -- 
+    // Exercice 1 : 
     Un_elem* stations; 
-    printf("\n Voici la liste des stations de métro : \n");
-    stations = lire_station("station_metro.csv")
-    
-    // question 4 : 
-    Une_coord *limite_no = (Une_coord*)malloc(sizeof(Une_coord);
+    stations = lire_stations("csv/stations_metro.csv");
+     
+    Une_coord *limite_no = (Une_coord*)malloc(sizeof(Une_coord));
+    if(!limite_no)
+    {
+        printf("probleme allocation mémoire\n");
+        return 1;
+    }
     limite_no->lon = 2000; 
     limite_no->lat = -1000;                                           
-    Une_coord *limite_se = (Une_coord*)malloc(sizeof(Une_coord);
+    Une_coord *limite_se = (Une_coord*)malloc(sizeof(Une_coord));
+    if(!limite_se)
+    {
+        printf("probleme allocation mémoire\n");
+        return 1;
+    }
     limite_no->lon = -2000; 
     limite_no->lat = 1000;
     limites_zone(stations, limite_no, limite_se);                                           
 
-    // -- Exercice 2 --
-    Un_nabr* arbre = (Un_nabr*)malloc(sizeof(Un_nabr);
-    arbre = construire_abr(stations);
+    //Exercice 2 :
+    Un_nabr* abr_sta = (Un_nabr*)malloc(sizeof(Un_nabr));
+    abr_sta = construire_abr(stations);
                                                                  
-    // -- Exercice 3 --                                 
+    // Exercice 3 :                                 
     Une_ligne* liste_ligne; 
-    printf("\n Voici la liste des lignes de métro : \n");                                  
-    liste_ligne = lire_ligne("../lignes_metro.csv");                                  
+    liste_ligne = lire_ligne("csv/lignes_metro.csv");                                  
                                       
-    // -- Exercice 4 --
+    // Exercice 4 :
     Un_elem* connexions; 
-    printf("\n Voici la liste des connexions des stations de métro : \n");                                  
-    connexions = lire_connexion("connexions.csv")
-    int compt = 0; //initialisation du compteur à 0                                  
-    Un_elem* head = stations; 
-    while (!head)/*tant que head n'est pas nul*/
-      {   
-          char* nom = head->truc>data.sta.nom; 
-          Un_truc* new = checher_station(arbre, nom);
-          head->truc= new; 
-          head = head->suiv; 
-          compt++; 
-      }
+    connexions = lire_connexions("csv/connexions.csv",liste_ligne , abr_sta);
+    // int compt = 0; //initialisation du compteur à 0                                  
+    // Un_elem* head = stations; 
+    // while (!head)
+    // {   
+    //     char* nom = head->truc>data.sta.nom; 
+    //     Un_truc* new = checher_station(arbre, nom);
+    //     head->truc= new; 
+    //     head = head->suiv; 
+    //     compt++; 
+    // }
                                                                        
-    // -- Exercice 6 -- 
-    int a; 
-    char* sta_dep; 
-    sta_dep = (char*)malloc(sizeof(char));  
-    printf("\n Veuillez entrer votre station de départ :\n");   
-    gets(sta_dep); 
-    while((a = getchar()) != '\n' && a != EOF); /*condition dans le but d'attendre le scanf*/
-    Un_truc* truc_sta_dep = chercher_station(arbre, sta_dep);
+    // // Exercice 6 : 
+    // int c; 
+    // char* sta_dep; 
+    // sta_dep = (char*)malloc(sizeof(char));  
+    // printf("\n Veuillez entrer votre station de départ :\n");   
+    // gets(sta_dep); 
+    // while((c = getchar()) != '\n' && c != EOF);
+    // Un_truc* truc_sta_dep = chercher_station(arbre, sta_dep);
                                                                    
-    char* sta_arr; 
-    sta_arr = (char*)malloc(sizeof(char));  
-    printf("\n Veuillez entrer votre station d'arrivée :\n");   
-    gets(sta_arr); 
-    while((a = getchar()) != '\n' && a != EOF) 
-    Un_truc* truc_sta_arr = chercher_station(arbre, sta_arr);      
+    // char* sta_arr; 
+    // sta_arr = (char*)malloc(sizeof(char));  
+    // printf("\n Veuillez entrer votre station d'arrivée :\n");   
+    // gets(sta_arr); 
+    // while((c = getchar()) != '\n' && c != EOF);
+    // Un_truc* truc_sta_arr = chercher_station(arbre, sta_arr);      
 
-    int chemin_voulu; 
-    printf("Veuillez entrer si vous préférez le chemin le plus court (1) ou le plus rapide (0) :\n");                                 
-    scanf("%d", &d); 
+    // int chemin_voulu; 
+    // printf("Veuillez entrer si vous préférez le choix le plus court (1) ou le plus rapide (0) :\n");                                 
+    // scanf("%d", chemin_voulu); 
                                       
-    printf("\n Le trajet pour aller de la station %s à la station %s est :\n", sta_dep, sta_arr);
-    dijkstra(stations,truc_sta_dep,connexions,chemin_voulu); 
-    Un_elem* chemin = cherche_chemin(truc_sta_arr);
-    ecrire_chemin(chemin, connexions, chemin_voulu); 
+    // printf("\n Le trajet pour aller de %s à %s est :\n", sta_dep, sta_arr);
+    // dijkstra(stations,truc_sta_dep,connexions,chemin_voulu); 
+    // Un_elem* chemin = cherche_chemin(truc_sta_arr);
+    // ecrire_chemin(chemin, connexions, chemin_voulu); 
                                       
-    // Désallocation de la mémoire : 
-    free(limite_no); 
-    free(limite_se);                                  
-    free(sta_dep); 
-    free(sta_arr); 
-    detruire_liste_et_truc(connexions);
-    detruire_lignes(liste_ligne); 
-    detruire_truc(truc_sta_dep);                                  
-    detruire_truc(truc_sta_arr); 
-    detruire_abr(arbre);                                 
+    // // Désallocation de la mémoire : 
+    // free(limite_no); 
+    // free(limite_se);                                  
+    // free(sta_dep); 
+    // free(sta_arr); 
+    // detruire_liste_et_truc(connexions);
+    // detruire_lignes(liste_ligne); 
+    // detruire_truc(truc_sta_dep);                                  
+    // detruire_truc(truc_sta_arr); 
+    // detruire_abr(arbre);                                 
                                       
    return 0;
     
